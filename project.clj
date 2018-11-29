8399cd31c5177f6205d27f4c1241ef9d68f3e72f
liquidz <liquidz.uo@gmail.com>
2018-11-29 15:24:36 +0900

    Bump nrepl to 0.5.0

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [nrepl "0.4.5"]]
+                 [nrepl "0.5.0"]]
   :profiles
   {:dev {:source-paths ["dev" "src"]}})
