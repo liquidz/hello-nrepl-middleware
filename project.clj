2d16209cb2aa27c14d79c467ff1e4d018766cde2
liquidz <liquidz.uo@gmail.com>
2018-12-18 15:03:52 +0900

    Bump dependencies

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [nrepl "0.5.2"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [nrepl "0.5.3"]]
   :profiles
   {:dev {:source-paths ["dev" "src"]}})
